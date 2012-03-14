f7fa265c762dff967cdd349e43990bb775fbbb5e
Gert Jan van Hardeveld <gj@gj-VirtualBox.(none)>
2012-03-13 23:14:52 -0500

    url, view, template

--- settings.py
+++ settings.py
@@ -13,8 +13,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'democrat',                      # Or path to database file if using sqlite3.
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -105,9 +103,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'democratorrepublican.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    "/home/gj/democratorrepublican/templates"
 )
 
 INSTALLED_APPS = (
--- urls.py
+++ urls.py
@@ -1,13 +1,11 @@
 from django.conf.urls.defaults import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'democratorrepublican.views.home', name='home'),
-    # url(r'^democratorrepublican/', include('democratorrepublican.foo.urls')),
+    url(r'^$', 'democratorrepublican.party.views.homepage', name='homepage'),
+    #url(r'^democratorrepublican/', include('democratorrepublican.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
